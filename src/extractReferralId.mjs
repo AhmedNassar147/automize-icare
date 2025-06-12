49e176da00c1efef955db7ea3b1bc0fcfd6b151e
AhmedNassar147 <nassarahmed750@gmail.com>
2025-06-13 00:20:31 +0300

    work on initial test

--- src/extractReferralId.mjs
+++ src/extractReferralId.mjs
@@ -6,8 +6,8 @@
 const extractReferralId = (text) => {
   if (!text) return "";
 
-  const [, referralId] = text.match(/\*Referral ID\*:\s*([^\n\r]+)/) || [];
-  return referralId;
+  const [, referralId] = text.match(/🔢 \*Referral ID:\* (\d+)/) || [];
+  return referralId || "";
 };
 
 export default extractReferralId;
